41ffa7dff82f7f650e6c734bb7c060ee40d71e23
Madhumitha <madhumithavenu03@gmail.com>
2023-12-08 11:49:30 +0530

    creating index file and adding contents from bootstramp

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import { Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Logout from './components/Logout'
+import Signup from './components/Signup'
+import AddBlogs from './components/AddBlogs'
+import UserBlogs from './components/UserBlogs'
+import BlogDetail from './components/BlogDetail'
+import Blog from './components/Blog'
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    }
+    this.setLoggedIn = this.setLoggedIn.bind(this);
+  }
+
+  setLoggedIn() {
+    this.setState()
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar isLoggedIn={this.state.isLoggedIn} setLoggedIn={this.setLoggedIn} />
+        <Routes>
+          <Route path='/auth' element={<Login setLoggedIn={this.setLoggedIn} />} />
+          <Route path='/auth/logout' element={<Logout setLoggedIn={this.setLoggedIn} />} />
+          <Route path='/auth/signup' element={<Signup setLoggedIn={this.setLoggedIn} />} />
+
+          <Route path='/' element={<Home />} />
+          <Route path='/blog/:id' element={<Blog />} />
+          <Route path='/blog/add' element={<AddBlogs isLoggedIn={this.state.isLoggedIn} />} />
+          <Route path='/myBlogs' element={<UserBlogs />} />
+          <Route path='/editBlog/:id' element={<BlogDetail />} />
+        </Routes>
+        <Footer />
+
+      </>
+    )
+  }
+}
+
+export default App
