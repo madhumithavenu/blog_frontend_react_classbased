fc5fab4cee38b40eac2553389c5c2e51e779bdf4
Madhumitha <madhumithavenu03@gmail.com>
2023-12-08 13:15:46 +0530

    creating blog details

---
+++ src/components/BlogDetails.jsx
@@ -0,0 +1,138 @@
+import axios from 'axios';
+import React, { Component } from 'react'
+
+export class BlogDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputs: {
+        title: "",
+        description: "",
+        image: ""
+      }
+    }
+    this.sendRequest = this.sendRequest.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchDetails = this.fetchDetails.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState((prevState) => ({
+      inputs: {
+        ...prevState.inputs,
+        [e.target.name]: e.target.value,
+      }
+    }))
+  }
+
+  async fetchDetails(id) {
+    const res = await axios.get(`http://localhost:5000/api/blog/${id}`)
+      .catch(err => console.log("Data not getting"))
+
+    let data = null;
+    if (res) {
+      data = await res.data;
+      console.log(data);
+    }
+    return data;
+  }
+
+  async componentDidMount() {
+    const idArr = document.URL.split('/');
+    let id = idArr[idArr.length - 1];
+
+    this.fetchDetails(id)
+      .then(data => this.setState({
+        inputs: {
+          title: data.blog.title,
+          description: data.blog.description,
+          image: data.blog.image,
+        }
+      }))
+  }
+
+  async sendRequest(id) {
+    const res = await axios.put(`http://localhost:5000/api/blog/update/${id}`, {
+      title: this.state.inputs.title,
+      description: this.state.inputs.description,
+      image: this.state.inputs.image
+    })
+      .catch(err => console.log(err))
+
+    let data = null;
+    if (res) {
+      data = await res.data;
+      console.log(data);
+    }
+    return data;
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.inputs);
+    // console.log(this.state.blog);
+    const idArr = document.URL.split('/');
+    let id = idArr[idArr.length - 1];
+    this.sendRequest(id)
+      .then(data => console.log(data))
+      .then(() => {
+        window.location.replace("/myBlogs")
+      })
+      .catch(() => console.log("Can not update"))
+  }
+
+
+  render() {
+    return (
+      <>
+        <header className="masthead" style={{ "backgroundImage": "url(" + this.state.inputs.image + ")" }}>
+          <div className="container position-relative px-4 px-lg-5">
+            <div className="row gx-4 gx-lg-5 justify-content-center">
+              <div className="col-md-10 col-lg-8 col-xl-7">
+                <div className="page-heading">
+                  <h1>{this.state.inputs.title}</h1>
+                  <span className="subheading">{this.state.inputs.description}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        {/* Post Content*/}
+        {this.state.inputs &&
+          <div className="container px-4 px-lg-5">
+            <div className="row gx-4 gx-lg-5 justify-content-center">
+              <div className="col-md-10 col-lg-8 col-xl-7">
+                <form action="/blogs" method="POST" onSubmit={this.handleSubmit} >
+                  <div className="form-floating">
+                    <input className="form-control" id="title" name="title" type="text"
+                      placeholder="Enter the title..." value={this.state.inputs.title} onChange={this.handleChange} />
+                    <label htmlFor="title">Title</label>
+                  </div>
+
+                  <div className="form-floating">
+                    <textarea className="form-control" id="description" name="description"
+                      placeholder="Enter your description here..." value={this.state.inputs.description} onChange={this.handleChange}></textarea>
+                    <label htmlFor="description">Description</label>
+                  </div>
+                  <div className="form-floating">
+                    <textarea className="form-control" id="image" name="image"
+                      placeholder="Add Image..." value={this.state.inputs.image} onChange={this.handleChange}></textarea>
+                    <label htmlFor="image">Image</label>
+                  </div> <br />
+
+                  {/* Submit Button*/}
+                  <div style={{ textAlign: 'center' }}>
+                    <button id="submitButton" className="btn btn-primary text-uppercase" type="submit">Update</button>
+                  </div><br />
+                </form>
+              </div>
+            </div>
+          </div>
+        }
+      </>
+    )
+  }
+}
+
+export default BlogDetail
